c9b5e2a1fbacc05ef1e96bee8031d18706aeccb3
kuzaleks <kuzAleksAleks@gmail.com>
2017-06-04 21:50:49 +0300

    fix mistakes

--- src/main/java/bsu/diplom/effects/Effect.java
+++ src/main/java/bsu/diplom/effects/Effect.java
@@ -7,34 +7,13 @@ public abstract class Effect {
 
     public Double[] transform(Double[] sig) {
         int bufLen = sig.length;
-//        while (bufLen <= sig.length) {
-//            bufLen *= 2;
-//        }
-
-//        bufLen /= 2;
-
         Double[] trans = new Double[bufLen];
         for (int i = 0; i < bufLen; i++)
             trans[i] = effect(i, sig, bufLen);
 
         return trans;
     }
-
-    public Double[] doubleTransform(Double[] sig1, Double[] sig2) {
-        int bufLen = sig1.length;
-//        while (bufLen <= sig.length) {
-//            bufLen *= 2;
-//        }
-
-//        bufLen /= 2;
-
-        Double[] trans = new Double[bufLen];
-        for (int i = 0; i < bufLen; i++)
-            trans[i] = effect(i, sig1, bufLen);
-
-        return trans;
-    }
-
+    
     public long getSampleFreq() {
         return sampleFreq;
     }
